7ce7e2bc0db2d3aebad66bccb02f970148f2d376
Abdelkader Lounis <abdouaallb@gmail.com>
2022-03-10 16:39:22 +0100

    adding dummy data file to the project

---
+++ src/data/index.js
@@ -0,0 +1,126 @@
+export const aboutGdSc = [{ id: 1, title: "", description: "" }];
+
+export const aboutGdscUsthb = [
+  { id: 1, title: "", description: "" },
+  { id: 2, title: "", description: "" },
+  { id: 3, title: "", description: "" },
+];
+
+export const fieldsOfWork = [
+  { id: 1, title: "", description: "" },
+  { id: 2, title: "", description: "" },
+  { id: 3, title: "", description: "" },
+  { id: 4, title: "", description: "" },
+  { id: 5, title: "", description: "" },
+];
+
+export const events = [
+  { id: 1, avatar: "", from: "", to: "", title: "", description: "" },
+  { id: 2, title: "", avatar: "", from: "", to: "", description: "" },
+  { id: 3, title: "", avatar: "", from: "", to: "", description: "" },
+];
+
+export const membersOfTheMonth = [
+  {
+    id: 1,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 2,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 3,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 4,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 5,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 6,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 7,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 8,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 9,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+  {
+    id: 10,
+    firstName: "",
+    lastName: "",
+    role: "",
+    description: "",
+    socials: [{ id: 1, linkedin: "", twitter: "", instagram: "" }],
+  },
+];
+
+export const projects = [
+  { id: 1, title: "", description: "", avatar: "" },
+  { id: 2, title: "", description: "", avatar: "" },
+  { id: 3, title: "", description: "", avatar: "" },
+];
+
+export const trustedBy = [
+  { id: 1, companyName: "", description: "", avatar: "" },
+  { id: 2, companyName: "", description: "", avatar: "" },
+  { id: 3, companyName: "", description: "", avatar: "" },
+  { id: 4, companyName: "", description: "", avatar: "" },
+  { id: 5, companyName: "", description: "", avatar: "" },
+  { id: 6, companyName: "", description: "", avatar: "" },
+];
+
+export const questions = [
+  { id: 1, question: "", answer: "" },
+  { id: 2, question: "", answer: "" },
+  { id: 3, question: "", answer: "" },
+  { id: 4, question: "", answer: "" },
+];
