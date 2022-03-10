4d0875785b26a1e36e39ec7ebe2219486df6f6a9
Abdelkader Lounis <abdouaallb@gmail.com>
2022-03-10 19:54:16 +0100

    added an api file that returns landing page data

---
+++ src/api/landingPage/index.js
@@ -0,0 +1,33 @@
+import * as data from "../../data/index";
+
+export const getDataAboutGdsc = () => {
+  return data.aboutGdSc;
+};
+
+export const getDataAboutGdscUsthb = () => {
+  return data.aboutGdscUsthb;
+};
+
+export const getDataFieldsOfWork = () => {
+  return data.fieldsOfWork;
+};
+
+export const getDataEvents = () => {
+  return data.events;
+};
+
+export const getDataMembers = () => {
+  return data.membersOfTheMonth;
+};
+
+export const getDataProjects = () => {
+  return data.projects;
+};
+
+export const getDataTrustedBy = () => {
+  return data.trustedBy;
+};
+
+export const getDataQuestions = () => {
+  return data.questions;
+};
